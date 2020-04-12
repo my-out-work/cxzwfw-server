0b0e22909b108f04172c054ac511c6b64ebc048f
minjs.cn <zm@tuia.cn>
2020-04-13 07:10:17 +0800

    cache days=1

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const APP_NAME = pkg.name.split('-')[0]
 app.keys = [pkg.name]
 const session_config = {
   key: 'koa:' + APP_NAME, /**  cookie的key。 (默认是 koa:sess) */
-  maxAge: 3 * 24 * 60 * 60 * 1000,   /**  session 过期时间，以毫秒ms为单位计算 。*/
+  maxAge: 1 * 24 * 60 * 60 * 1000,   /**  session 过期时间，以毫秒ms为单位计算 。*/
   autoCommit: true, /** 自动提交到响应头。(默认是 true) */
   overwrite: true, /** 是否允许重写 。(默认是 true) */
   httpOnly: true, /** 是否设置HttpOnly，如果在Cookie中设置了"HttpOnly"属性，那么通过程序(JS脚本、Applet等)将无法读取到Cookie信息，这样能有效的防止XSS攻击。  (默认 true) */
